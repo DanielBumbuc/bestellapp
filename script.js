404016cf072ca63b14ccabd766b8e097ebc1d906
Daniel Bumbuc <bumbucd@gmail.com>
2024-10-11 22:05:45 +0200

    add Cart Script and delete Script

--- script.js
+++ script.js
@@ -5,33 +5,59 @@ function init() {
 function renderDishCategory() {
     for (categoryIndex = 0; categoryIndex < dishes.length; categoryIndex++) {
         let contentRef = document.getElementById('order_container');
-            contentRef.innerHTML += getDishCategory(categoryIndex);
-            renderDishName(categoryIndex);
+        contentRef.innerHTML += getDishCategory(categoryIndex);
+        renderDishName(categoryIndex);
     }
 }
 
 function renderDishName(categoryIndex) {
     for (dishIndex = 0; dishIndex < dishes[categoryIndex].dish.length; dishIndex++) {
         let contentRef = document.getElementById('dish_category' + categoryIndex);
-            contentRef.innerHTML += getDishName(dishIndex);
+        contentRef.innerHTML += getDishName(dishIndex);
     }
 }
 
-function addDish(dishIndex) {
-    let cartContainer = document.getElementById('cart_container');
-    let myDishName = document.getElementById('dish_name' + dishIndex).innerHTML;
-    let myDishPrice = document.getElementById('dish_price' + dishIndex).innerHTML;
-    cartContainer.innerHTML += `<div class="my-current-cart">
-                <p>${myDishName}</p>
-                <p>${myDishPrice}</p>
-                <input type="number" value="1">
-            </div>`;
+function addDish(categoryIndex, dishIndex) {
+    let myDishName = document.getElementById(categoryIndex + 'dish_name' + dishIndex).innerHTML;
+    let myDishPrice = document.getElementById(categoryIndex + 'dish_price' + dishIndex).innerHTML;
+    cartDishesArr.push({ 'name': myDishName, 'price': myDishPrice });
+    renderCartDishes();
 }
 
-// after add dish amount '1' with option +/- '1'
+function renderCartDishes() {
+    let cartContainer = document.getElementById('cart_container');
+    cartContainer.innerHTML = '';
+    for (cartDishIndex = 0; cartDishIndex < cartDishesArr.length; cartDishIndex++) {
+        cartContainer.innerHTML += getCartDishes(cartDishIndex);
+    }
+}
+
+function calcPlusAmount() {
+    let dishAmount = +document.getElementById('dish_amount').innerHTML;
+    let plusAmount = dishAmount + 1;
+    let amountValue = document.getElementById('dish_amount');
+    amountValue.innerHTML = plusAmount;
+}
+
+function calcMinusAmount() {
+    let dishAmount = +document.getElementById('dish_amount').innerHTML;
+    let minusAmount = dishAmount - 1;
+    let amountValue = document.getElementById('dish_amount');
+    amountValue.innerHTML = minusAmount;
+    if (amountValue.innerHTML < 1) {
+        delete cartDishes.name;
+    }
+
+}
+
+function deleteCart(cartDishIndex) {
+    cartDishesArr.splice(cartDishIndex, 1);
+    renderCartDishes();
+    console.log(cartDishesArr);
+
+}
+
 // if amount < 1 delete from cart
 // delete complete dish button
 // price: subtotal + delivery costs + total sum (if pickup then no delivery costs)
-
-// set projekt in github
 
--- scripts/template.js
+++ scripts/template.js
@@ -6,16 +6,30 @@ function getDishCategory(categoryIndex) {
 }
 
 function getDishName(dishIndex) {
-    let dishName = `<div id="all_dishes${dishIndex}" class="all-dishes">
+    let dishName = `<div id="${categoryIndex}all_dishes${dishIndex}" class="all-dishes">
         <div id="my_dish_id${dishIndex}">
-            <p id="dish_name${dishIndex}">${dishes[categoryIndex].dish[dishIndex].name}</p>
+            <p id="${categoryIndex}dish_name${dishIndex}">${dishes[categoryIndex].dish[dishIndex].name}</p>
             <p id="dish_ingredients${dishIndex}">${dishes[categoryIndex].dish[dishIndex].ingredients}</p>
-            <p id="dish_price${dishIndex}">${dishes[categoryIndex].dish[dishIndex].price}</p>
+            <p id="${categoryIndex}dish_price${dishIndex}">${dishes[categoryIndex].dish[dishIndex].price}</p>
         </div>
         <div>
-            <img onclick="addDish(${dishIndex})" class="add-button" src="./assets/icons/icons8-hinzufügen-50.png" alt="add-button">
+            <img onclick="addDish(${categoryIndex}, ${dishIndex})" class="add-button" src="./assets/icons/icons8-hinzufügen-50.png" alt="add-button">
         </div>
     </div>`
     return dishName;
 }
 
+function getCartDishes(cartDishIndex) {    
+    let cartDishes = `<div class="my-current-cart">
+                <p>${cartDishesArr[cartDishIndex].name}</p>
+                <p>${cartDishesArr[cartDishIndex].price}</p>
+                <div>
+                    <img onclick="calcMinusAmount()" class="minus-button" id="minus" src="./assets/icons/icons8-minus-64.png" alt="minus-button">
+                    <span id="dish_amount">1</span>
+                    <img onclick="calcPlusAmount()" id="plus" class="plus-button" src="./assets/icons/icons8-hinzufügen-50.png" alt="plus-button">
+                    <img onclick="deleteCart(${cartDishIndex})" src="./assets/icons/icons8-delete-48.png" alt="delete">
+                </div>
+            </div>`;
+
+    return cartDishes
+}
